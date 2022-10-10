039e16543b3bb705bbc26a7b1efbedd192b3a0e8
sclo <sclo@gms.ndhu.edu.tw>
2022-10-10 14:18:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/inventorystarter/AddItemFragment.kt
@@ -0,0 +1,38 @@
+package com.example.inventory
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.inventorystarter.databinding.FragmentAddItemBinding
+
+
+class AddItemFragment : Fragment() {
+
+    lateinit var binding: FragmentAddItemBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentAddItemBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.saveAction.setOnClickListener {
+
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/inventorystarter/ItemDetailFragment.kt
@@ -0,0 +1,36 @@
+package com.example.inventory
+
+import android.content.ClipData
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.inventorystarter.databinding.FragmentItemDetailBinding
+
+
+class ItemDetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentItemDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentItemDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
